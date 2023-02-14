a6a194e5a44a72600b42f29ef1a2aa7082bec2bd
Rafael Martins Machado <rafaelmartinsmachado25@gmail.com>
2023-02-14 14:05:49 -0300

    exercicio3-concluido

--- lab-tube/src/App.js
+++ lab-tube/src/App.js
@@ -2,11 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+const titulo = "LabeTube"
+const tituloDoVideo = "Título do Vídeo"
+
+function reproduzVideo() {
+  alert("O vídeo está sendo reproduzido")
+}
+
+
+
   return (
     <div>
        <div className="tela-inteira">
         <header>
-            <h1>LabeTube</h1>
+            <h1>{titulo}</h1>
             <input type="text" placeholder="Busca" id="campoDeBusca"/>
         </header>
 
@@ -24,37 +34,37 @@ function App() {
             </nav>
 
             <section className="painel-de-videos">
-                <div className="box-pagina-principal media1" onclick="reproduzVideo()">
+                <div className="box-pagina-principal media1" onClick={() => reproduzVideo()}>
                     <img src="https://picsum.photos/400/400?a=1 " alt=""/>
-                    <h4>Título do vídeo</h4>
+                    <h4>{tituloDoVideo}</h4>
                 </div>
-                <div className="box-pagina-principal media2" onclick="reproduzVideo()">
+                <div className="box-pagina-principal media2" onClick={() => reproduzVideo()}>
                     <img src="https://picsum.photos/400/400?a=2 " alt=""/>
-                    <h4>Título do vídeo</h4>
+                    <h4>{tituloDoVideo}</h4>
                 </div>
-                <div className="box-pagina-principal media3" onclick="reproduzVideo()">
+                <div className="box-pagina-principal media3" onClick={() => reproduzVideo()}>
                     <img src="https://picsum.photos/400/400?a=3 " alt=""/>
-                    <h4>Título do vídeo</h4>
+                    <h4>{tituloDoVideo}</h4>
                 </div>
-                <div className="box-pagina-principal media4" onclick="reproduzVideo()">
+                <div className="box-pagina-principal media4" onClick={() => reproduzVideo()}>
                     <img src="https://picsum.photos/400/400?a=4 " alt=""/>
-                    <h4>Título do vídeo</h4>
+                    <h4>{tituloDoVideo}</h4>
                 </div>
-                <div className="box-pagina-principal media5" onclick="reproduzVideo()">
+                <div className="box-pagina-principal media5" onClick={() => reproduzVideo()}>
                     <img src="https://picsum.photos/400/400?a=5 " alt=""/>
-                    <h4>Título do vídeo</h4>
+                    <h4>{tituloDoVideo}</h4>
                 </div>
-                <div className="box-pagina-principal media6" onclick="reproduzVideo()">
+                <div className="box-pagina-principal media6" onClick={() => reproduzVideo()}>
                     <img src="https://picsum.photos/400/400?a=6 " alt=""/>
-                    <h4>Título do vídeo</h4>
+                    <h4>{tituloDoVideo}</h4>
                 </div>
-                <div className="box-pagina-principal media7" onclick="reproduzVideo()">
+                <div className="box-pagina-principal media7" onclick={() => reproduzVideo()}>
                     <img src="https://picsum.photos/400/400?a=7 " alt=""/>
-                    <h4>Título do vídeo</h4>
+                    <h4>{tituloDoVideo}</h4>
                 </div>
-                <div className="box-pagina-principal media8" onclick="reproduzVideo()">
+                <div className="box-pagina-principal media8" onClick={() => reproduzVideo()}>
                     <img src="https://picsum.photos/400/400?a=8 " alt=""/>
-                    <h4>Título do vídeo</h4>
+                    <h4>{tituloDoVideo}</h4>
                 </div>
             </section>
         </main>
